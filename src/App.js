dde8b0dfe8ef4aedab149685ddf649a0bba8beaf
Diego Cardoso <diegocar448@hotmail.com>
2022-08-24 13:46:29 -0300

    Abordagem 1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import CustomerForm from './components/CustomerForm';
+import Hello from './components/Hello';
+import './App.css';
+
+
+function App() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleSubmit = ({name, email}) => {
+    console.log('dados APP', {name, email});
+    
+    setName(name);
+    setEmail(email);
+  }
+
+  return (
+    <div className="app">
+      <Hello name={ name }/>
+      <CustomerForm onSubmit={handleSubmit}/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/CustomerForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const CustomerForm = ({ onSubmit }) => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('dados componente', {name, email});
+
+        onSubmit({ name, email });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Nome: </label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                value={name}
+                onChange={ (e) => setName(e.target.value) } />
+            <label htmlFor="email">Email: </label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={ (e) => setEmail(e.target.value) } />
+
+            <button type="submit">Enviar</button>
+        </form>
+    )
+}
+
+export default CustomerForm;
